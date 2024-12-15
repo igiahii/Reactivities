8fc43523bd6b55066d6081d01111e86a0021bb7f
igiahii <giahiiman@gmail.com>
2024-12-15 16:00:33 +0330

    made some changes and refactor front side

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import Navbar from "./NavBar";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
